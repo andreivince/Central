3dc9d9f52c4ad8604627bbec2efa597221e965cf
andreivince <andreivince21@gmail.com>
2023-02-03 08:22:13 -0500

    contact changes, put names and a click to clipboard the gmail

---
+++ script.js
@@ -0,0 +1,7 @@
+function myFunction() {
+    // Get the text field
+    var copyText = document.getElementById("click");
+  
+    navigator.clipboard.writeText("andreivince21@gmail.com")
+    alert("Copy on clipboard")
+}
